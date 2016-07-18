ee8be9cd8c0f1b4c5013c065905f9ff84b51d94b
Alexander Fu <aafu@ucdavis.edu>
2016-07-18 00:53:14 -0700

    Prelim push

--- calc.js
+++ calc.js
@@ -9,75 +9,75 @@ String.prototype.toProperCase = function () {
 
 
 var pokeEvo = {
-	"abra" :"Kadabras", 
-	"bellsprout" :"Weepinbells", 
-	"bulbasaur" :"Ivysaurs", 
-	"caterpie" :"Metapods", 
-	"charmander" :"Charmeleons", 
-	"charmeleon" :"Charmanders", 
-	"clefairy" :"Clefables", 
-	"cubone" :"Marowaks", 
-	"diglett" : "Dugtrios", 
-	"doduo" :"Dodrios", 
-	"dragonair" :"Dragonites", 
-	"dratini" :"Dragonairs", 
-	"drowzee" :"Hypnos", 
-	"eevee" :"Eevee evolved forms", 
-	"ekans" :"Arboks", 
-	"exeggcute" :"Exeggutors", 
-	"gastly" :"Haunters", 
-	"geodude" :"Gravelers", 
-	"gloom" :"Vileplumes", 
-	"goldeen" :"Seakings", 
-	"graveler" :"Golems", 
-	"grimer" :"Mucks", 
-	"growlithe" :"Growlites", 
-	"haunter" :100, 
-	"horsea" :50, 
-	"ivysaur" :100, 
-	"jigglypuff" :50, 
-	"kabuto" :50, 
-	"kadabra" :100, 
-	"kakuna" :50, 
-	"koffing" :50, 
-	"krabby" :50, 
-	"machoke" :100, 
-	"machop" :25, 
-	"magikarp" :400, 
-	"magnemite" :50, 
-	"mankey" :50, 
-	"meowth" :50, 
-	"metapod" :50, 
-	"nidoran♀" :25, 
-	"nidoran♂" :25, 
-	"nidorina" :100, 
-	"nidorino" :100, 
-	"oddish" :25, 
-	"omanyte" :50, 
-	"paras" :50, 
-	"pidgeotto" :50, 
-	"pidgey" :12, 
-	"pikachu" :50, 
-	"poliwag" :25, 
-	"poliwhirl" :100, 
-	"ponyta" :50, 
-	"psyduck" :50, 
-	"rattata" :25, 
-	"rhyhorn" :50, 
+	"abra" :"Kadabra", 
+	"bellsprout" :"Weepinbell", 
+	"bulbasaur" :"Ivysaur", 
+	"caterpie" :"Metapod", 
+	"charmander" :"Charmeleon", 
+	"charmeleon" :"Charmander", 
+	"clefairy" :"Clefable", 
+	"cubone" :"Marowak", 
+	"diglett" : "Dugtrio", 
+	"doduo" :"Dodrio", 
+	"dragonair" :"Dragonite", 
+	"dratini" :"Dragonair", 
+	"drowzee" :"Hypno", 
+	"eevee" :"Eevee evolved form", 
+	"ekans" :"Arbok", 
+	"exeggcute" :"Exeggutor", 
+	"gastly" :"Haunter", 
+	"geodude" :"Graveler", 
+	"gloom" :"Vileplume", 
+	"goldeen" :"Seaking", 
+	"graveler" :"Golem", 
+	"grimer" :"Muck", 
+	"growlithe" :"Growlite", 
+	"haunter" :"Gengar", 
+	"horsea" :"Seadra", 
+	"ivysaur" :"Venasaur", 
+	"jigglypuff" :"Wigglytuff", 
+	"kabuto" :"Kabutop", 
+	"kadabra" :"Alakazam", 
+	"kakuna" :"Beedrill", 
+	"koffing" :"Wheezing", 
+	"krabby" :"Kingler", 
+	"machoke" :"MAchamp", 
+	"machop" :"Machop", 
+	"magikarp" :"Gyarado", 
+	"magnemite" :"Magnetite", 
+	"mankey" :"Primeape", 
+	"meowth" :"Persian", 
+	"metapod" :"Buterfree", 
+	"nidoran♀" :"Nidorina", 
+	"nidoran♂" :"Nidorino", 
+	"nidorina" :"Nidoqueen", 
+	"nidorino" :"Nidoking", 
+	"oddish" :"Gloom", 
+	"omanyte" :"Omastar", 
+	"paras" :"Parasect", 
+	"pidgeotto" :"Pidgeot", 
+	"pidgey" :"Pidgeotto", 
+	"pikachu" :"Raichu", 
+	"poliwag" :"Poliwhirl", 
+	"poliwhirl" :"Poliwrath", 
+	"ponyta" :"Rapidash", 
+	"psyduck" :"Golduck", 
+	"rattata" :"Raticate", 
+	"rhyhorn" :"Rhydon", 
 	"sandshrew" :"Sandslash", 
-	"seel" :"Dewongs", 
-	"shellder" :"Cloysters", 
-	"slowpoke" :"Slowbrows", 
-	"spearow" :"Fearows", 
-	"squirtle" :"Wartortles", 
-	"staryu" :"Starmes", 
-	"tentacool" :"Tentacruels", 
-	"venonat" :"Venomoths", 
-	"voltorb" :"Electrodes", 
-	"vulpix" :"Ninetaless", 
-	"wartortle" :"Blastoises", 
-	"weedle" : "Kakunas", 
-	"weepinbell" :"Victreebels", 
+	"seel" :"Dewong", 
+	"shellder" :"Cloyster", 
+	"slowpoke" :"Slowbrow", 
+	"spearow" :"Fearow", 
+	"squirtle" :"Wartortle", 
+	"staryu" :"Starme", 
+	"tentacool" :"Tentacruel", 
+	"venonat" :"Venomoth", 
+	"voltorb" :"Electrode", 
+	"vulpix" :"Ninetales", 
+	"wartortle" :"Blastoise", 
+	"weedle" : "Kakuna", 
+	"weepinbell" :"Victreebel", 
 	"zubat" :"Golbats"
 }
 
@@ -156,11 +156,11 @@ var pokeDict = {
 }
 
 function generateTransferString(num, pok) {
-	return "Transfer <b>" + num + " " + pok + "<b> to Willow." 
+	return "<li>Transfer <b>" + num + " " + pok + "s</b> to Willow.</li>" 
 }
 
-function generateTransferString2(num, pok) {
-	return "Transfer <b>" + num + " " + pok + "<b> to Willow." 
+function generateEvolveString(num, pok) {
+	return "<li>Evolve <b>" + num + " " + pok + "s</b>.</li>" 
 }
 
 // Error codes:
@@ -169,7 +169,7 @@ function generateTransferString2(num, pok) {
 
 function processRow(name, count, candies, hasEvo, recycle) {
 	var cost = pokeDict[name.toLowerCase()];
-	if(!cost)
+	if(!cost || !count || !candies)
 		return {"status": -1}
 
 
@@ -223,9 +223,9 @@ function processRow(name, count, candies, hasEvo, recycle) {
 		origTransfersRequired = totalTransfersRequired;
 	}
 
-	var pokeDexReward = totalTransfersRequired > 0 ? 1 : 0;
+	var pokeDexReward = totalTransfersRequired > 0 && !hasEvo ? 1 : 0;
 
-	return {"evoTransfers": evoTransfersRequired , "origTransfers":origTransfersRequired, "hasReward":pokeDexReward, "pureEvos": pureEvoLimitedByNumberOfPokemon, "transferEvos": transferEvoUsingCandy}
+	return {"status": 0, "evoTransfers": evoTransfersRequired , "origTransfers":origTransfersRequired, "hasReward":pokeDexReward, "pureEvos": pureEvoLimitedByNumberOfPokemon, "transferEvos": transferEvoUsingCandy}
 	// console.log(totalTransfersRequired)
 	// console.log("Evo transfers")
 	// console.log(evoTransfersRequired)
@@ -235,12 +235,83 @@ function processRow(name, count, candies, hasEvo, recycle) {
 
 
 function runAll() {
-	console.log($("#firstPokemon").val());
-	// $('.tableRow').each(function(i, obj) {
- //  		console.log($(obj).find("input:eq(2)").val());
-	// });
+	// console.log($("#firstPokemon").val());
+	var origExp = 0;
+	var totalTransfers = 0;
+	var totalEvolutions = 0;
+	var timeNeeded = 0;
+	var setupActionList = Array();
+	var postEggActionList = Array();
+	$('.tableRow').each(function(i, obj) {
+		var name = $(obj).find("input:eq(0)").val();
+		var inven = $(obj).find("input:eq(1)").val();
+		var own = $(obj).find("input:eq(2)").val();
+		var cc1 = $(obj).find("input:eq(3)").is(':checked');
+		var cc2 = $(obj).find("input:eq(4)").is(':checked');
+		console.log(cc1, cc2)
+		var ret = processRow(name, inven, own, cc1, cc2);
+		if (ret["status"] == -1) {
+			$(obj).css("background", "#DB4D4D");
+		} else {
+			$(obj).css("background", "None");
+		}
 
-}
+		console.log(ret);
+		if(ret["origTransfers"]) {
+			totalTransfers += ret["origTransfers"];
+			setupActionList.push(generateTransferString(ret["origTransfers"], name));
+		}
+
+
+		if(!ret["evoTransfers"]) {
+
+				if(ret["pureEvos"] + ret["transferEvos"]) {
+					totalEvolutions += ret["pureEvos"] + ret["transferEvos"];
+					origExp += 500 * (ret["pureEvos"] + ret["transferEvos"]);
+					timeNeeded += 0.25 * (ret["pureEvos"] + ret["transferEvos"]);
+					postEggActionList.push(generateEvolveString(ret["pureEvos"] + ret["transferEvos"], name));
+
+				}
+
+		} else {
+
+				if(ret["pureEvos"]) {
+					totalEvolutions += ret["pureEvos"];
+					origExp += 500;
+					timeNeeded += 0.25 * ret["pureEvos"];
+					postEggActionList.push(generateEvolveString(ret["pureEvos"], name));
+
+				}
+
+
+				if(ret["evoTransfers"]) {
+					totalTransfers += ret["evoTransfers"];
+					timeNeeded += 0.10 * ret["evoTransfers"];
+					postEggActionList.push(generateTransferString(ret["evoTransfers"], pokeEvo[name.toLowerCase()]));
+				}
+
+				if(ret["transferEvos"]) {
+					totalEvolutions += ret["transferEvos"];
+					origExp += 500;
+					timeNeeded += 0.25 * ret["transferEvos"];
+					postEggActionList.push(generateEvolveString(ret["transferEvos"], name));
+				}
+
+		}
+		
+		if(ret["hasReward"]) {
+			origExp += 500;
+		}
+	});
+	$("#instructions").removeClass("hidden");
+
+	$("#instlist").html(($.parseHTML( setupActionList.join("") + "<li>Activate <b>LUCKY EGGO</b>.</li>"  +  postEggActionList.join("") )));
+	$("#totxp").html(origExp * 2)
+	$("#xpw").html(origExp)
+	$("#time").html(timeNeeded)
+	$("#trans").html(totalTransfers)
+	$("#evo").html(totalEvolutions)
+}	
 
 
 var pokeList = Array();
@@ -250,25 +321,32 @@ for (var a in pokeDict) {
 var counter = 2;
 
 $( document ).ready(function() {
-	$("#firstPokemon").bind('input', function() { 
-		// console.log("AAAA");
-		var self = this;
-	    if (self.timer)
-	        clearTimeout(self.timer);
-
-	    self.timer = setTimeout(function ()
-	    {
-	        self.timer = null;
-	        runAll();
-	    }, 500);
-    	// throttle(runAll, 500);
+	$("#runProgram").click(function() { 
+		runAll();
 	});
 
 	// $('#input-1').keypress( _.debounce( function(){
 	// 	runAll();
 	// }, 500 ) );
 
-  	$("#firstPokemon" ).autocomplete({
+  	$("#pok-0" ).autocomplete({
+  		source: pokeList,
+		select: function (event, ui) {
+		    return false;
+		},
+
+		select: function (event, ui) {
+		    $(this).val(ui.item ? ui.item : " ");
+		},
+
+		change: function (event, ui) {
+		    if (!ui.item) {
+		        this.value = '';}
+		}
+
+	});
+
+  	$("#pok-1" ).autocomplete({
   		source: pokeList,
 		select: function (event, ui) {
 		    return false;
@@ -286,9 +364,43 @@ $( document ).ready(function() {
 	});
 
   	$( "#addRow" ).click(function() {
-  		console.log("AAAAA")
-  		var a = $("#input-1").clone().attr("id", "input-" + counter++);
+  			
+  		var a = $("#input-1").clone().attr("id", "input-" + counter);
+  		$(a).find("input:eq(0)").val("").attr("id", "pok-" + counter);
+  		$(a).find("input:eq(1)").val("");
+  		$(a).find("input:eq(2)").val("");
   		$("#rowBody").append(a);
+
+		$("#pok-" + counter).bind('input', function() { 
+			// console.log("AAAA");
+			var self = this;
+		    if (self.timer)
+		        clearTimeout(self.timer);
+
+		    self.timer = setTimeout(function ()
+		    {
+		        self.timer = null;
+		        runAll();
+		    }, 500);
+	    	// throttle(runAll, 500);
+		});
+
+
+	  	$("#pok-" + counter++ ).autocomplete({
+	  		source: pokeList,
+			select: function (event, ui) {
+			    return false;
+			},
+
+			select: function (event, ui) {
+			    $(this).val(ui.item ? ui.item : " ");
+			},
+
+			change: function (event, ui) {
+			    if (!ui.item) {
+			        this.value = '';}
+			}
+		});
 	});
 
 });
